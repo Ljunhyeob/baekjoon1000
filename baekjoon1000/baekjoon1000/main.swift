bd177c3be602bf05f7b5e2aaf936c70512170920
이준협 <121070166+Ljunhyeob@users.noreply.github.com>
2023-01-03 10:40:49 +0900

    Add files via upload

---
+++ baekjoon1000/baekjoon1000/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  baekjoon1000
+//
+//  Created by 이준협 on 2023/01/03.
+//
+
+import Foundation
+
+
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+let c = a+b
+print(c)
+
